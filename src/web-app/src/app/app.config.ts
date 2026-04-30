0730ed57339f3b7106ada1d5904b458a9a208e8f
Oliver Corsino <oliverdiaz19@gmail.com>
2026-04-30 16:27:11 -0400

    - Refactor dashboard style to add top navar - Adds the correct header to the table. - Moves the buy button to the dashboard.

--- src/web-app/src/app/app.config.ts
+++ src/web-app/src/app/app.config.ts
@@ -20,7 +20,10 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([authInterceptor])),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+          darkModeSelector: 'none'
+        }
       }
     }),
 
--- src/web-app/src/app/presentation/dashboard/dashboard.ts
+++ src/web-app/src/app/presentation/dashboard/dashboard.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SelectModule } from 'primeng/select';
 import { IconFieldModule } from 'primeng/iconfield';
@@ -7,33 +8,42 @@ import { MultiSelectModule } from 'primeng/multiselect';
 import { Table, TableModule } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 import { Product } from '../../core/domain/models/product.model';
 import { ProductHandler } from '../../application/product/product.handler';
+import { AuthStore } from '../../application/auth/auth.store';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SelectModule, IconFieldModule, InputIconModule, MultiSelectModule, TableModule, TagModule, InputTextModule, FormsModule],
+  imports: [CommonModule, SelectModule, IconFieldModule, InputIconModule, MultiSelectModule, TableModule, TagModule, InputTextModule, FormsModule, ButtonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Dashboard implements OnInit {
   readonly productHandler = inject(ProductHandler);
+  readonly authStore = inject(AuthStore);
   
-  loading: boolean = true;
   products!: Product[];
+  isLoading = this.productHandler.isLoading;
 
   ngOnInit(): void {
     this.productHandler.getProducts().subscribe({
       next: (products) => {
         this.products = products;
-        this.loading = false;
         console.log('Products loaded successfully:', products);
       },
       error: (err) => {
         console.error('Failed to load products:', err);
-        this.loading = false;
       }
     });
+  }
+
+  logout(): void {
+    this.authStore.logout();
+  }
+
+  buy() {
+    this.productHandler.purchaseCorn();
   }
 }
